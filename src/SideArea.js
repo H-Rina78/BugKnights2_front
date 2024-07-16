433898446b8315ae5e14bbe52c050a7d582faa6f
hashimoto <lanshimaoli810@gmail.com>
2024-07-16 16:32:35 +0900

    カテゴリ

--- src/SideArea.js
+++ src/SideArea.js
@@ -2,6 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import './sideArea.css';
+import Carousel from 'react-bootstrap/Carousel';
 
 const SideArea = (props) => {
     //サイドエリアのどのボタンを押したかの情報を切り替え
@@ -22,6 +23,35 @@ const SideArea = (props) => {
     const testStyle2 = {
         color: 'green'
     }
+
+    const slides = [
+        {
+          id: 1,
+          imageUrl: 'https://bugknights.blob.core.windows.net/products/田中太郎.jfif',
+          caption: 'Slide 1',
+        },
+        {
+          id: 2,
+          imageUrl: 'https://bugknights.blob.core.windows.net/products/佐藤花子.jfif',
+          caption: 'Slide 2',
+        },
+        {
+          id: 3,
+          imageUrl: 'https://bugknights.blob.core.windows.net/products/鈴木一郎.jfif',
+          caption: 'Slide 3',
+        },
+        {
+          id: 4,
+          imageUrl: 'https://bugknights.blob.core.windows.net/products/高橋美咲.jfif',
+          caption: 'Slide 4',
+        },
+        {
+          id: 5,
+          imageUrl: 'https://bugknights.blob.core.windows.net/products/中村健太.jfif',
+          caption: 'Slide 5',
+        },
+      ];
+    
 
     return(
         <>
@@ -62,6 +92,29 @@ const SideArea = (props) => {
                             飲み物
                         </button>
                     </Col>
+
+                    <Col className="col-6 ms-4 ps-4 h5" style={Number(props.inputCategoryId) === 6 ? testStyle : testStyle2}>
+                        <button onClick={changeCategory} value={6}>
+                            果物
+                        </button>
+                    </Col>
+
+                    <Col className="col-6 ms-4 ps-4 h5" style={Number(props.inputCategoryId) === 7 ? testStyle : testStyle2}>
+                        <button onClick={changeCategory} value={7}>
+                            惣菜
+                        </button>
+                    </Col>
+                </Row>
+                <Row className='row mt-4'>
+                    <Carousel>
+                        {slides.map((slide) => (
+                            <Carousel.Item key={slide.id}>
+                            <a href='https://reservebugnights.azurewebsites.net/' target="_blank" rel="noopener noreferrer">
+                                <img className="d-block w-100" src={slide.imageUrl} />
+                            </a>
+                            </Carousel.Item>
+                        ))}
+                    </Carousel>
                 </Row>
             </Container>
         </>        

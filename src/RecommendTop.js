456480f47b0407db2a05cdca3a705e2b18badb46
hashimoto <lanshimaoli810@gmail.com>
2024-07-16 16:14:25 +0900

    recommend

--- src/RecommendTop.js
+++ src/RecommendTop.js
@@ -1,18 +1,17 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 
-const RecomendTop = () =>{
-    const RecomendTopStyle = {
+const RecommendTop = () =>{
+    const RecommendTopStyle = {
         backgroundColor:'#eaeaea'
     }
     return(
         <Container fluid>
-            <Row>
+            <Row style={RecommendTopStyle}>
                 <h1>おすすめの商品</h1>
             </Row>
         </Container>
     )
 }
 
-export default RecomendTop;
+export default RecommendTop;

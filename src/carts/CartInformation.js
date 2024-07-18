3158567ccd619c6392496ff4278d09faca07548e
hashimoto <lanshimaoli810@gmail.com>
2024-07-18 12:55:49 +0900

    カート

--- src/carts/CartInformation.js
+++ src/carts/CartInformation.js
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import SimpleHeader from "../SimpleHeader";
-import { Col, Container } from "react-bootstrap";
-import { Row } from "react-bootstrap";
+import { Col, Container, Row, Form } from "react-bootstrap";
+import Button from 'react-bootstrap/Button';
 
 const CartInformation = () => {
     const navigate = useNavigate();
@@ -16,42 +16,54 @@ const CartInformation = () => {
         backgroundColor:'#ffffff'
     }
 
+    const underlineStyle = {
+        borderBottom: '1px solid #ccc',
+        paddingBottom: '8px',
+        marginBottom: '20px'
+    }
+
+    const name = {
+        fontSize: '30px'
+    }
+
+    const btnStyle = {
+        width:'100px',
+        height:'30px',
+        fontSize:'13px'
+    }
+
+    const selectStyle = {
+        width: '60px',  // プルダウンの幅を調整
+        fontSize: '12px',  // フォントサイズを小さくする
+    }
+
     return (
         <>
         <SimpleHeader />
         <Container fluid className='p-5' style={style}>
             <Row className="justify-content-center py-3 px-5"  style={style2}>
-                <Row>
-                <h1>カート</h1>
+                <Row style={underlineStyle} className="d-flex align-items-center justify-content-center">
+                    <Col className="col-9"><h1>カート</h1></Col>
+                    <Col className="h5 col-3">カートに入っている商品：3点</Col>
                 </Row>
-                <Row className="p-2 mt-3">
-                    <Col className="text-center">
-                        <img style={{ width: '15rem', height: '15rem' }} src='https://bugknights.blob.core.windows.net/products/田中太郎.jfif' alt='advertisement' />
+                <Row className="p-2 mt-3" style={underlineStyle}>
+                    <Col className="col-5 text-center">
+                        <img style={{ width: '15rem', height: '10rem' }} src='https://bugknights.blob.core.windows.net/products/tomato.jpg' alt='advertisement' />
                     </Col>
                     <Col>
-                        <Row>商品名</Row>
-                        <Row>金額</Row>
-                        <Row>商品名</Row>
+                        <Row className="pb-5"><p style={name}>トマト</p></Row>
+                        <Row style={{display: 'flex'}}>
+                            数量：
+                            <Form.Select className="ms-3 me-3" style={selectStyle} defaultValue="1">
+                                {[...Array(11).keys()].map((num) => (
+                                    <option key={num} value={num}>{num}</option>
+                                ))}
+                            </Form.Select>
+                        </Row>
                     </Col>
-                </Row>
-                <Row className="mt-3">
                     <Col className="text-center">
-                        <img style={{ width: '15rem', height: '15rem' }} src='https://bugknights.blob.core.windows.net/products/田中太郎.jfif' alt='advertisement' />
-                    </Col>
-                    <Col>
-                        <Row>商品名</Row>
-                        <Row>金額</Row>
-                        <Row>商品名</Row>
-                    </Col>
-                </Row>
-                <Row className="mt-3">
-                    <Col className="text-center">
-                        <img style={{ width: '15rem', height: '15rem' }} src='https://bugknights.blob.core.windows.net/products/田中太郎.jfif' alt='advertisement' />
-                    </Col>
-                    <Col>
-                        <Row>商品名</Row>
-                        <Row>金額</Row>
-                        <Row>商品名</Row>
+                        <Row className="pb-5">300円(税込 330円)</Row>
+                        <Button variant='primary' style={btnStyle}>削除</Button>
                     </Col>
                 </Row>
             </Row>

6e25aa5ba57e4ba7bc9ea92f83331f665a9e0542
hashimoto <lanshimaoli810@gmail.com>
2024-07-18 17:14:44 +0900

    まいぺーじ

--- src/mypage/MyPage.js
+++ src/mypage/MyPage.js
@@ -1,10 +1,33 @@
+import { Container, Row } from "react-bootstrap";
 import SimpleHeader from "../SimpleHeader";
 
 const MyPage = () =>{
+
+    const AllStyle = {
+        backgroundColor: "#eaeaea"
+    }
+
     return(
         <>
         <SimpleHeader />
-        <span>マイページ</span>
+        <Container style={AllStyle}>
+            <Row>
+                <span>マイページ</span>
+            </Row>
+            <Row>
+                <span>○○様</span>
+            </Row>
+            <Row>
+                <p>登録情報</p>
+                <Row>
+                    <p>基本情報</p>
+                    <span>氏名：</span>
+                    <span>メールアドレス：</span>
+                    <span>電話番号：</span>
+                    <span>住所：</span>
+                </Row>
+            </Row>
+        </Container>
         </>
     )
 }

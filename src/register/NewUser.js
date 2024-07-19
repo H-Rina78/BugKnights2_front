4d1493246f647854a7ed88964180535c71d2cf3f
kawano <kyosuke.1215@icloud.com>
2024-07-19 15:23:47 +0900

    ぷｓｈ

--- src/register/NewUser.js
+++ src/register/NewUser.js
@@ -1,12 +1,27 @@
 import SimpleHeader from "../SimpleHeader";
+import { Container, Button, Row, Col } from 'react-bootstrap';
+import { useNavigate } from "react-router-dom";
 
 const NewUser = () => {
+    const navigate = useNavigate();
+
+    const handleClick = () => navigate("/");
+
     return (
         <>
             <SimpleHeader />
-            <span className="h1">
-                新規ユーザー登録が完了しました。
-            </span>
+            <Container>
+                <Row>
+                    <Col className="h1 text-center">
+                        新規ユーザー登録が完了しました。
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Button></Button>
+                    </Col>
+                </Row>
+            </Container>
         </>
     );
 }

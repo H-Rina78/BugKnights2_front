a30c7c29406ec286d3a87b35ba119fbd60b42f06
hashimoto <lanshimaoli810@gmail.com>
2024-07-22 15:53:04 +0900

    User

--- src/register/EMailRevision.js
+++ src/register/EMailRevision.js
@@ -4,6 +4,7 @@ import { Container, Form, Button, Row, Col } from 'react-bootstrap';
 import SimpleHeader from '../SimpleHeader';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useNavigate} from "react-router-dom";
+import Modal from 'react-bootstrap/Modal';
 
 const EMailRevision = () => {  
     const navigate = useNavigate();
@@ -11,12 +12,65 @@ const EMailRevision = () => {
 
     const location = useLocation();
     const [mail, setMail] = useState(location.state.mailData.mail);
+    const [id] = useState(location.state.mailData.id);
 
+    console.log(id);
     console.log(mail);
 
     const handleSubmit = (event) => {
         event.preventDefault();
     };
+
+    const MyVerticallyCenteredModal = (props) => {
+        
+        const updateData = () => {
+            const formData = new FormData();
+            formData.append('mail',mail);
+            formData.append('id',id);
+            fetch('https://bugknights-b.azurewebsites.net/mailRevision', {
+                method: 'POST',
+                body: formData
+            })
+            .then(response => response.text())
+            .then(data => {
+                console.log(data);
+                navigate("/Mypage");
+            })
+            .catch(error => console.error(error));
+        }
+
+        return (
+          <Modal
+            {...props}
+            size="sm"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+          >
+            <Modal.Header closeButton>
+              <Modal.Title id="contained-modal-title-vcenter">
+                入力内容確認
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <Row>
+                <Col>
+                    <p>メールアドレス：{mail}</p>
+                </Col>
+              </Row>
+            </Modal.Body>
+            <Modal.Footer>
+                <Row>
+                    <Col className="text-center">
+                        <Button variant="primary" onClick={updateData} className="me-3">変更する</Button>
+                        <Button variant="danger" onClick={props.onHide}>キャンセル</Button>
+                    </Col>
+                </Row>
+            </Modal.Footer>
+          </Modal>
+        );
+      }
+
+    const [modalShow, setModalShow] = useState(false);
 
     const btnStyle = {
         hight:'20px',
@@ -33,21 +87,26 @@ const EMailRevision = () => {
                 <p style={{fontSize:'20px'}}>新しいメールアドレスを入力してください。</p>
             </Row>
             <Col className='my-3' xs={12} md={6}>
-                <Form>
+                <Form onSubmit={handleSubmit}>
                     <Form.Group className='mb-3' controlId="formBasicEmail">
                         <Form.Label>メールアドレス</Form.Label>
                         <Form.Control 
                             type="text" 
                             placeholder="E-mail"
                             value={mail}
+                            onChange={(e)=>setMail(e.target.value)}
                             required
                         />
                     </Form.Group>
                     <Row className="justify-content-center mb-3">
                         <Col xs={6} md={6}>
-                        <Button variant="primary" onClick={handleClickMypage} type="submit" className="w-100">
+                        <Button variant="primary" type="submit" onClick={()=>setModalShow(true)} className="w-100">
                             変更する
                         </Button>
+                        <MyVerticallyCenteredModal
+                            show={modalShow}
+                            onHide={() => setModalShow(false)}
+                        />
                         </Col>
                     </Row>
                 </Form>

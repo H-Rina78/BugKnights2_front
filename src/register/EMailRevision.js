0288771503e48ad68d841a5fb7ddc6d7b57908cf
hashimoto <lanshimaoli810@gmail.com>
2024-07-22 11:19:27 +0900

    mail

--- src/register/EMailRevision.js
+++ src/register/EMailRevision.js
@@ -1,14 +1,18 @@
-import React, { useState } from 'react';
+import { useEffect, useState } from "react";
+import { useLocation } from 'react-router-dom'
 import { Container, Form, Button, Row, Col } from 'react-bootstrap';
 import SimpleHeader from '../SimpleHeader';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const EMailRevision = () => {
+const EMailRevision = () => {  
+    const location = useLocation();
+    const [mail, setMail] = useState(location.state.mailData.mail);
 
-    const btnStyle = {
-        hight:'20px',
-        width:'100px'
-    }
+    console.log(mail);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    };
 
   return (
     <>
@@ -19,25 +23,26 @@ const EMailRevision = () => {
             <Row className="text-center">
                 <p style={{fontSize:'20px'}}>新しいメールアドレスを入力してください。</p>
             </Row>
-            <Col className='my-5' xs={12} md={6}>
+            <Col className='my-3' xs={12} md={6}>
                 <Form>
-                <Form.Group className='mb-3' controlId="formBasicEmail">
-                    <Form.Label>メールアドレス</Form.Label>
-                    <Form.Control 
-                    type="text" 
-                    placeholder="E-mail"
-                    required
-                    />
-                </Form.Group>
-                <Row className="justify-content-center mb-3">
-                    <Col xs={6} md={6}>
-                    <Button style={btnStyle} variant="primary" type="submit" className="w-100">
-                        変更する
-                    </Button>
-                    </Col>
-                </Row>
+                    <Form.Group className='mb-3' controlId="formBasicEmail">
+                        <Form.Label>メールアドレス</Form.Label>
+                        <Form.Control 
+                            type="text" 
+                            placeholder="E-mail"
+                            value={mail}
+                            required
+                        />
+                    </Form.Group>
+                    <Row className="justify-content-center mb-3">
+                        <Col xs={6} md={6}>
+                        <Button variant="primary" type="submit" className="w-100">
+                            変更する
+                        </Button>
+                        </Col>
+                    </Row>
                 </Form>
-          </Col>
+            </Col>
         </Row>
       </Container>
     </>

3eabe0224852658dafd960dbf1a29fd19de932cc
kawano <kyosuke.1215@icloud.com>
2024-07-23 12:12:46 +0900

    p

--- src/mains/MainInformation.js
+++ src/mains/MainInformation.js
@@ -29,7 +29,7 @@ const MainInformation = () => {
   useEffect(() => {
     // Cookieから既存のカート情報を読み込む
     if (cookies.loginSession !== undefined && cookies.loginSession !== null) {
-      fetch('http://localhost:8080/bk/checkLogin', {
+      fetch('bugknights-b.azurewebsites.net/bk/checkLogin', {
         method: 'GET',
         credentials: 'include'
       })

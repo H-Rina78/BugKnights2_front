968e1c96b0b055eabf0dcc9f9677995ef7406b60
kawano <kyosuke.1215@icloud.com>
2024-07-29 13:55:47 +0900

    push

--- src/payment/PaymentCompleted.js
+++ src/payment/PaymentCompleted.js
@@ -1,5 +1,7 @@
 import {Button} from "react-bootstrap";
 import { useNavigate} from "react-router-dom";
+import SimpleHeader from "../SimpleHeader";
+import { Container, Row, Col } from 'react-bootstrap';
 
 const PaymentCompleted = () =>{
 
@@ -13,8 +15,21 @@ const PaymentCompleted = () =>{
 
     return(
         <>
-        <h1>決済完了</h1>
-        <Button className="ms-3 my-2" onClick={handleClick} style={btnStyle}>TOPへ戻る</Button>
+            <SimpleHeader />
+            <Container>
+                <Row>
+                    <Col className="h1 text-center col-12">
+                        決済完了
+                    </Col>
+                    <Col className="fs-4 text-center col-12 my-3">
+                        <p>ご利用ありがとうございました。</p>
+                        <p>またのご利用お待ちしております。</p>
+                    </Col>
+                </Row>
+                <Row className="justify-content-center">
+                    <Button className="ms-3 my-2" onClick={handleClick} style={btnStyle}>TOPへ戻る</Button>
+                </Row>
+            </Container>
         </>
     )
 }
